8a0a2a4c0974e68dceaf09e58e51005243ece935
emilygabrielaacevedorosales-wq <emilygabrielaacevedorosales@gmail.com>
2026-03-31 04:42:38 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'ridersconnect-v1';
+const urlsToCache = ['/', '/index.html'];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(names) {
+            return Promise.all(
+                names.map(function(name) {
+                    if (name !== CACHE_NAME) return caches.delete(name);
+                })
+            );
+        })
+    );
+});
